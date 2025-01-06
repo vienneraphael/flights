569b490686e9d1feb7accf1ebfee5748befa0a14
lilian doublet <liliandoublet@gmail.com>
2025-01-06 08:56:29 +0100

    feat: add url generation function

--- src/flights/url.py
+++ src/flights/url.py
@@ -1,22 +1,39 @@
 from fast_flights import FlightData, Passengers, create_filter
 
 def generate_flight_url(
-        departure_date,
-        from_airport,
-        to_airport,
-        trip_type="one-way",
-        seat_type="economy",
-        adults=1,
-        children=0,
-        infants_in_seat=0,
-        infants_on_lap=0,
-        currency="EUR"
-):
-    
+        departure_date: str,
+        from_airport: str,
+        to_airport: str,
+        trip_type: str="one-way",
+        seat_type: str="economy",
+        adults: int=1,
+        children: int=0,
+        infants_in_seat: int=0,
+        infants_on_lap: int=0,
+        currency: str="EUR"
+) -> str:
+    """ 
+    Generate Flights URL form parameters.
+
+    Paraleters:
+    - departure_date: str,date of departure, formated as YYYY-DD-MM.
+    - from_airport: str, IATA code of the departure airport (e.g., "CDG".)
+    - to_airport: str, IATA code of the arrival airport (e.g., "JFK")
+    - trip_type: str, type of trip, either "one-way" or "round-trip" (default: "one-way").
+    - seat_type: str, class of service, e.g., "economy", "business" (default: "economy").
+    - adults: int, number of adult passengers (default: 1).
+    - children: int, number of child passengers (default: 0).
+    - infants in seat: int, number of infants sitting on laps ( default: 0).
+    - currency: str, currency for pricing (default: "EUR").
+
+    Returns:
+    - str, a URL string with the flight search parameters.
+    """
+        
     filter = create_filter(
         flight_data=[
             FlightData(
-                date = departure_date,
+                date=departure_date,
                 from_airport=from_airport,
                 to_airport=to_airport,
             )
@@ -35,8 +52,8 @@ def generate_flight_url(
     return f"https://www.google.com/travel/flights?tfs={b64}&curr={currency}"
 
 #Exemple
-
-url= generate_flight_url(
+def main():
+    url= generate_flight_url(
         departure_date="2025-28-03",
         from_airport="CDG",
         to_airport="KIX",
@@ -44,5 +61,8 @@ url= generate_flight_url(
         seat_type="economy",
         adults=2,
         children=1,
-)
-print(url)
+    )
+    print(url)
+
+if __name__=="__main__":
+    main()
