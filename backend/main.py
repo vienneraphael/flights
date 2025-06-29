d5e6c91ab0d59d7ff32dbb409516135146386563
Raphael Vienne <129849233+vienneraphael@users.noreply.github.com>
2025-06-29 16:48:35 +0200

    add CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.models.trip import Scenario, UserTrip
 from backend.utils import fetch_flights_from_urls
 
 load_dotenv(override=True)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
